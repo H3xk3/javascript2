33a48e7e4a35ab2e91ffb9be4f1a3972db79f956
H3xk3 <pascalhex@hotmail.com>
2024-02-09 08:59:40 +0100

    opdrachten mooier gemaakt

--- Opdrachten/mvc/script.js
+++ Opdrachten/mvc/script.js
@@ -1,9 +1,7 @@
 "use strict";
 (function() {
-    // Model
     let model = {
         boek: {},
-
         init: function() {
             this.boek = {
                 isbn: "01234567",
@@ -13,28 +11,21 @@
                 status: "aanwezig"
             };
         },
-
         setStatus: function(newStatus) {
             this.boek.status = newStatus;
         },
-
         getData: function() {
             return this.boek;
         }
     };
-
-    // View
     let view = {
         isbnInput: document.getElementById('isbn'),
         titelInput: document.getElementById('titel'),
         jaarInput: document.getElementById('jaar'),
         publisherInput: document.getElementById('publisher'),
         statusInput: document.getElementById('status'),
-
         init: function() {
-            // TODO: Eventuele view initialisatie
         },
-
         setView: function(book) {
             this.isbnInput.value = book.isbn;
             this.titelInput.value = book.titel;
@@ -43,31 +34,23 @@
             this.statusInput.value = book.status;
         }
     };
-
-    // Controller
     let controller = {
         init: function() {
             model.init();
             view.init();
-
             document.getElementById('btnOntleend').addEventListener('click', function() {
                 model.setStatus('ontleend');
                 let bookData = model.getData();
                 view.setView(bookData);
             });
-
             document.getElementById('btnTerugBinnen').addEventListener('click', function() {
                 model.setStatus('terug binnen');
                 let bookData = model.getData();
                 view.setView(bookData);
             });
-
-            // Uitvoeren van de setView uit de view
             let initialBookData = model.getData();
             view.setView(initialBookData);
         }
     };
-
-    // Start de applicatie
     controller.init();
 })();
